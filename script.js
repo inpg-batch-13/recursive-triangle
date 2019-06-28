02e9791e76692bee60cddd2799c67173253dbdce
rahmatrhd <kickyou44@gmail.com>
2019-06-28 19:18:05 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function recursiveTriangle(n) {
+  // your code here
+  
+}
+
+// test case
+console.log(recursiveTriangle(5))
+// *****
+// ****
+// ***
+// **
+// *
+
+console.log(recursiveTriangle(3))
+// ***
+// **
+// *
